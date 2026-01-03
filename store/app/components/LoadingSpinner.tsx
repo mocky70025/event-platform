81277e57c08b756a671df9ba8c610340a27d142b
mocky70025 <kmc2515@kamiyama.ac.jp>
2026-01-03 13:18:43 +0900

    Describe what you changed

--- store/app/components/LoadingSpinner.tsx
+++ store/app/components/LoadingSpinner.tsx
@@ -3,7 +3,7 @@
 export default function LoadingSpinner() {
   return (
     <div className="flex justify-center items-center min-h-52">
-      <div className="w-10 h-10 border-4 border-gray-200 border-t-store rounded-full animate-spin" />
+      <div className="w-10 h-10 border-4 border-gray-200 border-t-sky-500 rounded-full animate-spin" />
     </div>
   );
 }
